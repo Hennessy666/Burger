5a8ae972a8a1414f021206450b39ceba66fceebb
github_Hennessy666 <sonjahennessy@gmail.com>
2020-01-31 12:54:38 -0500

    finished project

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,78 @@
 const connection = require("../config/connection");
 
+
+function createQuarks(num){
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function translateSql(obj){
+    var arr = [];
+    for (var key in ob){
+        var value = ob[key];
+        if(Object.hasOwnProperty.call(ob, key)){
+            if(typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'" ;
+            }
+            arr.push(key + "=" + value)
+        }
+    }
+
 const orm = {
-    selectAll: function(table, cb){
+    selectAll: function (table, cb) {
         var dbQuery = "SELECT * FROM " + table + ";";
 
-        connection.query(dbQuery, function(err, res){
-            if(err){
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
                 throw err;
             }
             cb(res);
         });
     }
-    insertOne: function(table, cols, vals, cb){
-        var dbQuery = "INSERT INTO" + 
-        table + " (" + cols.toString() + ") " +
-         "VALUES" ( + createQuarks(vals.length) + 
-
-        ") ";
+    insertOne: function (table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO" +
+            table + " (" +
+             cols.toString() + ") " +
+            "VALUES"(+ 
+            createQuarks(vals.length) +
+                ") ";
 
         console.log(dbQuery);
-        connection.query(dbQuery, vals, function(err, res){
-            if(err){
+        connection.query(dbQuery, vals, function (err, res) {
+            if (err) {
                 throw err;
             }
             cb(res);
-        }); 
+        });
     }
-updateOne: function(table, objColVals, condition, cb){
-    var dbQuery = 
-    "UPDATE" + 
-    table + " SET " + translateSql(objColVals) + " S"
-}
+updateOne: function (table, objColVals, condition, cb) {
+        var dbQuery =
+            "UPDATE" +
+            table + " SET " +
+             translateSql(objColVals) + 
+            " WHERE " +
+            condition;
+
+            console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if(err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+        deleteOne: function(table, condition, cb) {
+            var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+            console.log(dbQuery);
+
+            connection.query(dbQuery, function(err, res){
+                if(err) {
+                    throw err;
+                }
+                cb(res);
+            });
+        }
 };
